a3c54b4c99705fb282f3f5af985c0db87beede94
Top Coder <topcoder.premiumware@gmail.com>
2023-03-26 13:50:24 +0300

    fixed build

--- shopping-cart-nextjs/saga/sagas.ts
+++ shopping-cart-nextjs/saga/sagas.ts
@@ -1,16 +1,16 @@
 import { put, call, takeEvery } from "redux-saga/effects";
 import { fetchProducts } from "./request";
-import { getProducts, setProducts } from "slices/productsSlice";
+import { getProducts, setProducts } from "@/slices/productsSlice";
 
 export function* getProductsData() {
-  try {
-    const { data } = yield call(fetchProducts);
-    yield put(setProducts(data));
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const { data } = yield call(fetchProducts);
+        yield put(setProducts(data));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function* watcherSaga() {
-  yield takeEvery(getProducts.type, getProductsData);
+    yield takeEvery(getProducts.type, getProductsData);
 }
