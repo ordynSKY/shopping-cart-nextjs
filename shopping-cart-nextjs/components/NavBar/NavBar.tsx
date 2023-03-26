8e6e6c958e7d6c67cc7301ea5db6c28e567828db
Top Coder <topcoder.premiumware@gmail.com>
2023-03-26 15:43:01 +0300

    added styled components

--- shopping-cart-nextjs/components/NavBar/NavBar.tsx
+++ shopping-cart-nextjs/components/NavBar/NavBar.tsx
@@ -1,29 +1,65 @@
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import { cartSvg } from "@/svg";
-import styles from "@/styles/NavBar.module.css";
 import { stateType } from "@/types";
+import styled from "styled-components";
 
 const NavBar = () => {
     const { cartTotalQuantity } = useSelector((state: stateType) => state.cart);
 
+    const Navbar = styled.nav`
+        height: 70px;
+        background-color: #00bfff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 4rem;
+        h2 {
+            color: white;
+            font-size: 40px;
+            @media (max-width: 375px) {
+                font-size: 30px;
+            }
+        }
+    `;
+
+    const CartIcon = styled.div`
+        display: flex;
+        align-items: flex-start;
+        color: white;
+    `;
+
+    const CartQuantity = styled.span`
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        height: 15px;
+        width: 15px;
+        background-color: #f15656;
+        border-radius: 50%;
+        font-size: 14px;
+        font-weight: 700;
+        color: black;
+        margin-left: 5px;
+    `;
+
     return (
         <>
-            <nav className={styles.navbar}>
+            <Navbar>
                 <Link href="/">
                     <h2>React Shop</h2>
                 </Link>
                 <Link href="/cart">
-                    <div className={styles.cartIcon}>
+                    <CartIcon>
                         {cartSvg}
-                        <span className={styles.cartQuantity}>
+                        <CartQuantity>
                             <span style={{ color: "white", fontSize: 10 }}>
                                 {cartTotalQuantity}
                             </span>
-                        </span>
-                    </div>
+                        </CartQuantity>
+                    </CartIcon>
                 </Link>
-            </nav>
+            </Navbar>
         </>
     );
 };
